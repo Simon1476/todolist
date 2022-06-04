e482f31cb1831cedef8e8f9bee434af2ad0e9678
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2022-06-04 23:44:29 +0900

    todo.js !!

--- js/todo.js
+++ js/todo.js
@@ -2,27 +2,33 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDOList = document.getElementById('todo-list');
 
-const toDos = [];
+const TODOS_KEY = 'todos';
+
+let toDos = [];
+
 function saveToDos() {
-    localStorage.setItem("todos", JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 
 function deleteToDo(event) {
    const li  = event.target.parentElement; // This li is a element li we want to delete when we click the delete button.
    li.remove();
+   toDos = toDos.filter((item) => item.id !== parseInt(li.id));
+   saveToDos();
 }
 
 
-function paintToDo(newTodo) {
+function paintToDo(newTodoObj) {
 
     const li = document.createElement("li");
     const span = document.createElement("span");
     const button = document.createElement("button");
 
+    li.id = newTodoObj.id;
+    span.innerText = newTodoObj.text;
     button.innerText = "❌";
     button.addEventListener('click', deleteToDo);
-    span.innerText = newTodo;
 
     li.appendChild(span);
     li.appendChild(button);
@@ -35,9 +41,22 @@ function handleToDOSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";  
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(), // why need ,? 
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos(); //Put ToDos into localStorage  
+    
 }
 
 toDoForm.addEventListener('submit', handleToDOSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null ) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
