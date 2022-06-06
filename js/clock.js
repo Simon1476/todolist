977aeb4f3fb6fe39c3b89a60d34aae5cca78fc15
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2022-06-06 15:29:32 +0900

    todolist + css

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,5 @@
 const clock = document.querySelector('#clock');
+const morning = document.querySelector('.morning');
 
 function getClock() {
     const date = new Date();
@@ -6,7 +7,13 @@ function getClock() {
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
 
-    clock.innerText =`${hours}:${minutes}:${seconds}`;
+    clock.innerText =`${hours}시 ${minutes}분 ${seconds}초`;
+
+    if( hours >= 0 && hours <=12 ){
+        morning.innerText="Good morning, ";
+    } else if(hours >= 13 && hours <=18){
+        morning.innerText="Good Afternoon, ";
+    } else morning.innerText="Good Evening, ";
 }
 
 getClock();
--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,24 @@
+const API_KEY = "572440beb4330e20ff0d9e7d14a4d3f8";
+
 function onGeoOk(position) {
-    
+
     console.log(position);
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live it", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const weatherSpan = document.querySelector('.weather');
+        const tempSpan = document.querySelector('.temp');
+        const locationSpan = document.querySelector('.location');
+        
+        weatherSpan.innerText = `Weather : ${data.weather[0].main}`;
+        tempSpan.innerText = `Temp : ${data.main.temp}`;
+        locationSpan.innerText = `Location : ${data.name}`;
+        
+        
+    });
 }
 
 function onGeoError() {
