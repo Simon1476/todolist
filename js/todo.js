33ca650423fe24c5b17613d4c9b345f188d0f8be
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2022-06-12 23:33:31 +0900

    todolist edit btn

--- js/todo.js
+++ js/todo.js
@@ -13,8 +13,41 @@ function saveToDos() {
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
-function changeToDo() {
+function changeToDo(li, span, newTodoObj) {
 
+    let id = parseInt(li.id);
+
+    
+    const inputElem = document.createElement('input');
+    inputElem.value = span.innerText;
+    inputElem.classList.add('edit_input');
+
+    inputElem.addEventListener('keypress',(e)=> {
+
+        if(e.key === 'Enter'){
+
+            span.innerText = inputElem.value;
+
+            // newTodoObj.text = span.innerText;
+            if(toDos[0].id === id) {
+               toDos[0].text = span.innerText;
+            }   else if (toDos[1].id === id) {
+                toDos[1].text = span.innerText;
+             }  else if (toDos[2].id === id) {
+                toDos[2].text = span.innerText;
+             }  else if (toDos[3].id === id) {
+                toDos[3].text = span.innerText;
+             }  else if (toDos[4].id === id) {
+                toDos[4].text = span.innerText;
+             }
+            saveToDos();
+            inputElem.classList.add('edit_hidden');
+
+        }
+    })
+
+    li.appendChild(inputElem);
+    
 }
 
 function deleteToDo(event) {
@@ -46,18 +79,18 @@ function paintToDo(newTodoObj) {
     button.innerText = "❌";
     changeBtn.innerHTML ='<i class="fa-solid fa-pencil"></i>';
 
+
     button.addEventListener('click', deleteToDo);
-    changeBtn.addEventListener('cllick', changeToDo);
+    changeBtn.addEventListener('click', function(){changeToDo(li, span, newTodoObj)});
 
     div.appendChild(button);
     div.appendChild(changeBtn);
     li.appendChild(span);
     li.appendChild(div);
+
     toDOList.appendChild(li);
     toDosCnt++; // to-do list하나 생성 시 todo-list값 증가 
     
-    
-   
 }
 
 function handleToDOSubmit(event) {
